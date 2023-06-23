07bee2fe03620948565b57785b5db724001b2ec9
Dmytry Bondariev <bondarievmd@gmail.com>
2023-06-23 13:25:01 +0300

    Added static files managing; updated requirements.txt

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
