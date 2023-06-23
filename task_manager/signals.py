b2fca1894728bf4d499864148bfffa9a42040591
Dmytry Bondariev <bondarievmd@gmail.com>
2023-06-23 20:24:59 +0300

    Added default user for customers to test service functionality

--- task_manager/signals.py
+++ task_manager/signals.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
@@ -20,3 +21,16 @@ def create_positions(sender, **kwargs):
 
         for choice in Position.POSITION_CHOICES:
             Position.objects.create(name=choice[0])
+
+
+@receiver(post_migrate)
+def create_test_worker(sender, **kwargs):
+    if sender.name == 'task_manager':
+        get_user_model().objects.all().delete()
+        position = Position.objects.get(name="Project Manager")
+        get_user_model().objects.create_user(
+            username="user",
+            password="Password12345",
+            first_name="lovely User",
+            position=position
+        )
